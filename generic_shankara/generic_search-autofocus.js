125bbfc57c94939d3e9e39ec871b61e6e8a74512
Hemanth Sharma <hi@designbrained.studio>
2024-04-04 07:07:17 +0530

    Added Search Autofocus.

---
+++ generic_shankara/generic_search-autofocus.js
@@ -0,0 +1,15 @@
+/**********
+  SEARCH AUTOFOCUS
+  */
+
+$("#search-box_open-button").click(function () {
+  setTimeout(() => {
+    document.getElementById("search").focus();
+  }, 500);
+});
+
+$("#search-box-mobile_open-button").click(function () {
+  setTimeout(() => {
+    document.getElementById("search-mobile").focus();
+  }, 500);
+});
